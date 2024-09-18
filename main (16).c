f156a2d3b4f47e430aa358d8093b1eb8cc9b4c10
Onkg <167712918+Onkg@users.noreply.github.com>
2024-09-18 16:35:22 +0200

    Update main (16).c
    Added bytes to EEPROM

--- main (16).c
+++ main (16).c
@@ -137,7 +137,9 @@ int main(void)
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3); // Start PWM on TIM3 Channel 3
 
   // TODO: Write all bytes to EEPROM using "write_to_address"
-  
+  for (int i = 0; i <6;i++){
+	  write_to_address(ADDRESS+i,bin_array[i]);
+  }
   
   /* USER CODE END 2 */
 
